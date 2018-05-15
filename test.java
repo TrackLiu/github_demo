6ed05b64b9a0ef61a13cdf51618035af3e6950c5
TrackLiu <15216691368@163.com>
2018-05-15 11:09:45 +0800

    test

---
+++ test.java
@@ -0,0 +1,13 @@
+package com.csii.pmis.website.action.bean;
+
+import java.util.List;
+
+public class test{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4564619117193673252L;
+	
+  
+}
